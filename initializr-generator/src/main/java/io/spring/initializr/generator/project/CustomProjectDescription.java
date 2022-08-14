7f8774d3eb27f50a3ba2e2b7389f40ac77957f24
Sayeed Anakkaparambil <sayeed@litmus7.com>
2022-08-14 19:36:02 +0530

    :recycle: Add Custom project description to handle custom request

---
+++ initializr-generator/src/main/java/io/spring/initializr/generator/project/CustomProjectDescription.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.initializr.generator.project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.spring.initializr.generator.project.MutableProjectDescription;
+import io.spring.initializr.generator.project.ProjectDescription;
+
+/**
+ * A custom {@link ProjectDescription} to convey the additional flags to
+ * contributors.
+ *
+ * @author Stephane Nicoll
+ */
+public class CustomProjectDescription extends MutableProjectDescription {
+
+	private Boolean dockerFlag = true;
+
+	private List<String> projectComponents = new ArrayList<>();
+
+	public CustomProjectDescription() {
+	}
+
+	CustomProjectDescription(CustomProjectDescription source) {
+		super(source);
+		this.dockerFlag=source.getDockerFlag();
+		this.projectComponents=source.getProjectComponents();
+	}
+
+	@Override
+	public CustomProjectDescription createCopy() {
+		return new CustomProjectDescription(this);
+	}
+
+	public Boolean getDockerFlag() {
+		return dockerFlag;
+	}
+
+	public void setDockerFlag(Boolean dockerFlag) {
+		this.dockerFlag = dockerFlag;
+	}
+
+	public List<String> getProjectComponents() {
+		return projectComponents;
+	}
+
+	public void setProjectComponents(List<String> projectComponents) {
+		this.projectComponents = projectComponents;
+	}
+
+}
