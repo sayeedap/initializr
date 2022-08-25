da56acfdad8c83207802cfc529125de0aa9b1ed2
Sayeed Anakkaparambil <sayeed@litmus7.com>
2022-08-25 14:59:08 +0530

    :books: mustache template

---
+++ initializr-generator-spring/src/main/resources/templates/java/configuration/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package {{packageName}};
+
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("{{appName}} Rest APIs",
+                "APIs for {{appName}}.",
+                "1.0",
+                "Terms of service",
+                new Contact("test", "{{url}}", "test@emaildomain.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
