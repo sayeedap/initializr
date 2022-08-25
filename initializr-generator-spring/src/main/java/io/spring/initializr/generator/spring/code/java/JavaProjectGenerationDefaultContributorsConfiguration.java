f93d1793e76b749fb3e1ce78b34407fa51949486
Sayeed Anakkaparambil <sayeed@litmus7.com>
2022-08-25 15:00:50 +0530

    :art: Format code

--- initializr-generator-spring/src/main/java/io/spring/initializr/generator/spring/code/java/JavaProjectGenerationDefaultContributorsConfiguration.java
+++ initializr-generator-spring/src/main/java/io/spring/initializr/generator/spring/code/java/JavaProjectGenerationDefaultContributorsConfiguration.java
@@ -17,6 +17,7 @@
 package io.spring.initializr.generator.spring.code.java;
 
 import java.lang.reflect.Modifier;
+import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,29 +27,27 @@ import io.spring.initializr.generator.condition.ConditionalOnPackaging;
 import io.spring.initializr.generator.language.Annotation;
 import io.spring.initializr.generator.language.Parameter;
 import io.spring.initializr.generator.language.java.JavaExpressionStatement;
-import io.spring.initializr.generator.language.java.JavaFieldDeclaration;
 import io.spring.initializr.generator.language.java.JavaMethodDeclaration;
 import io.spring.initializr.generator.language.java.JavaMethodInvocation;
 import io.spring.initializr.generator.language.java.JavaReturnStatement;
+import io.spring.initializr.generator.language.java.JavaReturnType;
 import io.spring.initializr.generator.language.java.JavaTypeDeclaration;
 import io.spring.initializr.generator.packaging.war.WarPackaging;
 import io.spring.initializr.generator.project.ProjectDescription;
 import io.spring.initializr.generator.spring.code.MainApplicationTypeCustomizer;
 import io.spring.initializr.generator.spring.code.ServletInitializerCustomizer;
 import io.spring.initializr.generator.spring.code.TestApplicationTypeCustomizer;
-import io.spring.initializr.generator.spring.code.components.ControllerCodeCustomizer;
-import io.spring.initializr.generator.spring.code.components.ControllerImplCodeCustomizer;
-import io.spring.initializr.generator.spring.code.components.ExceptionCodeCustomizer;
-import io.spring.initializr.generator.spring.code.components.RepositoryCodeCustomizer;
-import io.spring.initializr.generator.spring.code.components.ServiceCodeCustomizer;
-import io.spring.initializr.generator.spring.code.components.ServiceImplCodeCustomizer;
-import io.spring.initializr.generator.spring.code.components.UtilCodeCustomizer;
+import io.spring.initializr.generator.spring.code.java.components.ControllerCodeCustomizer;
+import io.spring.initializr.generator.spring.code.java.components.ControllerImplCodeCustomizer;
+import io.spring.initializr.generator.spring.code.java.components.ServiceCodeCustomizer;
+import io.spring.initializr.generator.spring.code.java.components.ServiceImplCodeCustomizer;
 
 /**
  * Default Java language contributors.
  *
  * @author Andy Wilkinson
  * @author Stephane Nicoll
+ * @author Sayeed
  */
 @Configuration
 class JavaProjectGenerationDefaultContributorsConfiguration {
@@ -57,19 +56,20 @@ class JavaProjectGenerationDefaultContributorsConfiguration {
 	MainApplicationTypeCustomizer<JavaTypeDeclaration> mainMethodContributor() {
 		return (typeDeclaration) -> {
 			typeDeclaration.modifiers(Modifier.PUBLIC);
-			typeDeclaration.addMethodDeclaration(
-					JavaMethodDeclaration.method("main").modifiers(Modifier.PUBLIC | Modifier.STATIC).returning("void")
-							.parameters(new Parameter("java.lang.String[]", "args"))
-							.body(new JavaExpressionStatement(
-									new JavaMethodInvocation("org.springframework.boot.SpringApplication", "run",
-											typeDeclaration.getName() + ".class", "args"))));
+			typeDeclaration.addMethodDeclaration(JavaMethodDeclaration.method("main")
+					.modifiers(Modifier.PUBLIC | Modifier.STATIC).returning(new JavaReturnType("void"))
+					.parameters(new Parameter("java.lang.String[]", "args"))
+					.body(new JavaExpressionStatement(
+							new JavaMethodInvocation("org.springframework.boot.SpringApplication", "run",
+									typeDeclaration.getName() + ".class", "args"))));
 		};
 	}
 
 	@Bean
 	TestApplicationTypeCustomizer<JavaTypeDeclaration> junitJupiterTestMethodContributor() {
 		return (typeDeclaration) -> {
-			JavaMethodDeclaration method = JavaMethodDeclaration.method("contextLoads").returning("void").body();
+			JavaMethodDeclaration method = JavaMethodDeclaration.method("contextLoads")
+					.returning(new JavaReturnType("void")).body();
 			method.annotate(Annotation.name("org.junit.jupiter.api.Test"));
 			typeDeclaration.addMethodDeclaration(method);
 		};
@@ -82,7 +82,6 @@ class JavaProjectGenerationDefaultContributorsConfiguration {
 	@ConditionalOnPackaging(WarPackaging.ID)
 	static class WarPackagingConfiguration {
 
-		// function and retrun okke evide kodukkanam
 		@Bean
 		ServletInitializerCustomizer<JavaTypeDeclaration> javaServletInitializerCustomizer(
 				ProjectDescription description) {
@@ -90,7 +89,7 @@ class JavaProjectGenerationDefaultContributorsConfiguration {
 				typeDeclaration.modifiers(Modifier.PUBLIC);
 				JavaMethodDeclaration configure = JavaMethodDeclaration.method("configure")
 						.modifiers(Modifier.PROTECTED)
-						.returning("org.springframework.boot.builder.SpringApplicationBuilder")
+						.returning(new JavaReturnType("org.springframework.boot.builder.SpringApplicationBuilder"))
 						.parameters(new Parameter("org.springframework.boot.builder.SpringApplicationBuilder",
 								"application"))
 						.body(new JavaReturnStatement(new JavaMethodInvocation("application", "sources",
@@ -101,121 +100,53 @@ class JavaProjectGenerationDefaultContributorsConfiguration {
 
 	}
 
-	// function and retrun okke evide kodukkanam
 	@Bean
 	@ConditionalOnComponents("controller")
 	ControllerCodeCustomizer<JavaTypeDeclaration> javaControllerCodeCustomizer(ProjectDescription description) {
 		return (typeDeclaration) -> {
 			typeDeclaration.modifiers(Modifier.PUBLIC);
-			JavaMethodDeclaration configure = JavaMethodDeclaration.method("test").modifiers(Modifier.PUBLIC)
-					.returning("org.springframework.http.ResponseEntity<String>")
-					.body(new JavaReturnStatement(new JavaMethodInvocation("application", "sources",
-							description.getApplicationName() + ".class")));
-
-			configure.annotate(Annotation.name("org.springframework.web.bind.annotation.GetMapping"));
-
+			JavaMethodDeclaration configure = JavaMethodDeclaration.method("getHelloWorld").modifiers(Modifier.PUBLIC)
+					.returning(new JavaReturnType("org.springframework.http.ResponseEntity",
+							Collections.singletonList("String")))
+					.body(new JavaReturnStatement(
+							new JavaMethodInvocation("org.springframework.http.ResponseEntity", "ok", "Hai")));
 			typeDeclaration.addMethodDeclaration(configure);
 
 		};
 	}
 
-	// function and retrun okke evide kodukkanam
 	@Bean
 	@ConditionalOnComponents("controller")
 	ControllerImplCodeCustomizer<JavaTypeDeclaration> javaControllerImplCodeCustomizer(ProjectDescription description) {
 		return (typeDeclaration) -> {
-			typeDeclaration.modifiers(Modifier.PRIVATE);
-			JavaMethodDeclaration configure = JavaMethodDeclaration.method("test").modifiers(Modifier.PROTECTED)
-					.returning("org.springframework.http.ResponseEntity<String>")
-
-					.body(new JavaReturnStatement(new JavaMethodInvocation("application", "sources",
-							description.getApplicationName() + ".class")));
+			typeDeclaration.modifiers(Modifier.PUBLIC);
+			JavaMethodDeclaration configure = JavaMethodDeclaration.method("getHelloWorld").modifiers(Modifier.PUBLIC)
+					.returning(new JavaReturnType("org.springframework.http.ResponseEntity",
+							Collections.singletonList("String")))
+					.body(new JavaReturnStatement(new JavaMethodInvocation(
+							new JavaMethodInvocation("org.springframework.http.ResponseEntity", "ok"), "body",
+							"\"Hello World\"")));
 			configure.annotate(Annotation.name("java.lang.Override"));
+			configure.annotate(Annotation.name("org.springframework.web.bind.annotation.GetMapping"));
 			typeDeclaration.addMethodDeclaration(configure);
 
 		};
 	}
 
-	// function and retrun okke evide kodukkanam
 	@Bean
 	@ConditionalOnComponents("service")
 	ServiceCodeCustomizer<JavaTypeDeclaration> javaServiceCodeCustomizer(ProjectDescription description) {
 		return (typeDeclaration) -> {
-//			typeDeclaration.modifiers(Modifier.PRIVATE);
-//			JavaMethodDeclaration configure = JavaMethodDeclaration.method("configure").modifiers(Modifier.PROTECTED)
-//					.returning("org.springframework.boot.builder.SpringApplicationBuilder")
-//					.parameters(
-//							new Parameter("org.springframework.boot.builder.SpringApplicationBuilder", "application"))
-//					.body(new JavaReturnStatement(new JavaMethodInvocation("application", "sources",
-//							description.getApplicationName() + ".class")));
-//
-//			typeDeclaration.addMethodDeclaration(configure);
-
+			typeDeclaration.modifiers(Modifier.PUBLIC);
 		};
 	}
 
-	// function and retrun okke evide kodukkanam
 	@Bean
 	@ConditionalOnComponents("service")
 	ServiceImplCodeCustomizer<JavaTypeDeclaration> javaServiceImplCodeCustomizer(ProjectDescription description) {
 		return (typeDeclaration) -> {
-//			typeDeclaration.modifiers(Modifier.PRIVATE);
-//			JavaMethodDeclaration configure = JavaMethodDeclaration.method("configure").modifiers(Modifier.PROTECTED)
-//					.returning("org.springframework.boot.builder.SpringApplicationBuilder")
-//					.parameters(
-//							new Parameter("org.springframework.boot.builder.SpringApplicationBuilder", "application"))
-//					.body(new JavaReturnStatement(new JavaMethodInvocation("application", "sources",
-//							description.getApplicationName() + ".class")));
-
-//			typeDeclaration.addMethodDeclaration(configure);
-
+			typeDeclaration.modifiers(Modifier.PUBLIC);
 		};
-	}
-
-	// function and retrun okke evide kodukkanam
-	@Bean
-	@ConditionalOnComponents("utils")
-	UtilCodeCustomizer<JavaTypeDeclaration> javaRepositoryCodeCustomizer(ProjectDescription description) {
-		return (typeDeclaration) -> {
-//			typeDeclaration.modifiers(Modifier.PRIVATE);
-//			JavaMethodDeclaration configure = JavaMethodDeclaration.method("configure").modifiers(Modifier.PROTECTED)
-//					.returning("org.springframework.boot.builder.SpringApplicationBuilder")
-//					.parameters(
-//							new Parameter("org.springframework.boot.builder.SpringApplicationBuilder", "application"))
-//					.body(new JavaReturnStatement(new JavaMethodInvocation("application", "sources",
-//							description.getApplicationName() + ".class")));
-//
-//		typeDeclaration.addMethodDeclaration(configure);
-
-		};
-	}
-
-	// function and retrun okke evide kodukkanam
-	@Bean
-	@ConditionalOnComponents("exception")
-	ExceptionCodeCustomizer<JavaTypeDeclaration> javaExceptionCodeCustomizer(ProjectDescription description) {
-		return (typeDeclaration) -> {
-			typeDeclaration.modifiers(Modifier.PRIVATE);
-			JavaFieldDeclaration messageDeclaration = JavaFieldDeclaration.field("message").modifiers(Modifier.PRIVATE)
-					.returning("java.lang.String");
-
-			JavaFieldDeclaration httpStatusDeclaration = JavaFieldDeclaration.field("httpStatus")
-					.modifiers(Modifier.PRIVATE).returning("org.springframework.http.HttpStatus");
-			typeDeclaration.addFieldDeclaration(messageDeclaration);
-			typeDeclaration.addFieldDeclaration(httpStatusDeclaration);
-
-//			JavaMethodDeclaration configure = JavaMethodDeclaration.method("test")
-//					.modifiers(Modifier.constructorModifiers())
-//
-//					.parameters(
-//							new Parameter("org.springframework.boot.builder.SpringApplicationBuilder", "application"))
-//					.body(new JavaReturnStatement(new JavaMethodInvocation("application", "sources",
-//							description.getApplicationName() + ".class")));
-//
-//			typeDeclaration.addMethodDeclaration(configure);
-
-		};
-
 	}
 
 }
