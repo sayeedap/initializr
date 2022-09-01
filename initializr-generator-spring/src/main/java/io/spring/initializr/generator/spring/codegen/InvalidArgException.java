e86ea5c7df5b001cd325a59792e376e77d006734
stefyjoshy <stefyjoshy96@gmail.com>
2022-09-01 11:57:14 +0530

    Updated swagger-code-generator

---
+++ initializr-generator-spring/src/main/java/io/spring/initializr/generator/spring/codegen/InvalidArgException.java
@@ -0,0 +1,17 @@
+package io.spring.initializr.generator.spring.codegen;
+
+
+public class InvalidArgException extends RuntimeException {
+
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2909218126690586733L;
+
+	public InvalidArgException(String message) {
+		super(message);
+	}
+
+}
